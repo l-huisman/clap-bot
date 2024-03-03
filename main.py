a6aa6c1b621adfa250d54c2bbc5bae13cba561f7
l-huisman <64755533+l-huisman@users.noreply.github.com>
2024-03-03 12:04:53 +0100

    Add Discord bot with clapper functionality

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands, tasks
+from datetime import datetime
+
+bot = commands.Bot(intents=discord.Intents.all(), command_prefix="")
+
+clap = ":clap_tone5:"
+channel_id = 726154801955602532
+
+
+@tasks.loop(seconds=60)
+async def clapper():
+    current_time = datetime.now().strftime("%H:%M")
+    hour = current_time.split(":")[0]
+    minute = current_time.split(":")[1]
+
+    if current_time == "00:00":
+        await bot.get_channel(channel_id).send(clap * 3)
+    elif current_time == "11:11" or current_time == "22:22":
+        await bot.get_channel(channel_id).send(clap * 2)
+    elif hour == minute or hour == minute[::-1]:
+        await bot.get_channel(channel_id).send(clap)
+    elif minute == "00":
+        await bot.get_channel(channel_id).send(clap)
+
+
+@bot.event
+async def on_ready():
+    await clapper.start()
+
+
+bot.run("MTIxMjgzOTcyNjE0MzM4MTU4NQ.GaoMf1.MfcjmTjSuVtMnjCaShP4iCnRfkCj-sRvSYmqhw")
