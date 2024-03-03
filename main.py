9bda1c178a0a949bf23339f8be82422f4fc1f8bf
l-huisman <64755533+l-huisman@users.noreply.github.com>
2024-03-03 16:26:54 +0100

    Fix clapper function to use local time

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ channel_id = 726154801955602532
 
 @tasks.loop(seconds=60)
 async def clapper():
-    current_time = datetime.now().strftime("%H:%M")
+    current_time = datetime.now().astimezone().strftime("%H:%M")
     hour = current_time.split(":")[0]
     minute = current_time.split(":")[1]
 
@@ -20,14 +20,8 @@ async def clapper():
         await bot.get_channel(channel_id).send(clap * 2)
     elif hour == minute or hour == minute[::-1]:
         await bot.get_channel(channel_id).send(clap)
-        await bot.get_channel(channel_id).send(
-            "I clapped because it's: " + current_time
-        )
     elif minute == "00":
         await bot.get_channel(channel_id).send(clap)
-        await bot.get_channel(channel_id).send(
-            "I clapped because it's: " + current_time
-        )
 
 
 @bot.event
