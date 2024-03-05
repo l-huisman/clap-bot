e146392954e8e6d9fefd927190725a2dc286d419
l-huisman <64755533+l-huisman@users.noreply.github.com>
2024-03-05 22:14:21 +0100

    Refactor ClapBot class and update main.py

--- main.py
+++ main.py
@@ -4,84 +4,98 @@ from discord.ext import commands, tasks
 from dotenv import load_dotenv
 from datetime import datetime
 
-bot = commands.Bot(intents=discord.Intents.all(), command_prefix="")
-
 load_dotenv()
 
 token = os.getenv("DISCORD_TOKEN")
-channel_id = os.getenv("CHANNEL_ID")
+channel_id = int(os.getenv("CHANNEL_ID"))
 
-clap = ":clap_tone5:"
-shame_message_id = 0
-
-
-async def clap(times_to_clap: int) -> None:
-    if 0 < times_to_clap < 4:
-        await bot.get_channel(channel_id).send(clap * times_to_clap)
+# ClapBot Class
 
 
-async def check_for_clap_amount() -> int:
-    current_time = datetime.now().astimezone().strftime("%H:%M")
-    hour, minute = current_time.split(":")[0], current_time.split(":")[1]
-    return get_claps(current_time, hour, minute)
+class ClappyBoy(commands.Bot):
+    def __init__(self, command_prefix: str, intents: discord.Intents):
+        super().__init__(command_prefix=command_prefix, intents=intents)
+        self.clap_message = ":clap_tone5:"
+        self.shame_message = None
 
+    async def clap(self, times_to_clap: int) -> None:
+        if 0 < times_to_clap < 4:
+            await self.get_channel(channel_id).send(
+                (self.clap_message + " ") * times_to_clap
+            )
 
-def get_claps(current_time, hour, minute):
-    clap_times = {"00:00": 3, "11:11": 2, "22:22": 2}
-    if current_time in clap_times:
-        return clap_times[current_time]
-    elif hour == minute or hour == minute[::-1] or minute == "00":
-        return 1
-    else:
-        return 0
+    async def check_for_clap_amount(self) -> int:
+        current_time = datetime.now().astimezone().strftime("%H:%M")
+        hour, minute = current_time.split(":")[0], current_time.split(":")[1]
+        return self.get_claps(current_time, hour, minute)
 
+    def get_claps(self, current_time, hour, minute):
+        clap_times = {"00:00": 3, "11:11": 2, "22:22": 2}
+        if current_time in clap_times:
+            return clap_times[current_time]
+        elif hour == minute or hour == minute[::-1] or minute == "00":
+            return 1
+        else:
+            return 0
 
-async def shame_sender(self, user_id, message):
-    channel = bot.get_channel(channel_id)
-    user = channel.guild.get_member(user_id)
-    if user:
-        await message.channel.send(
-            f"<@{user_id}> you should be ashamed of yourself, clapping at this time of day is forbidden following article four of the all-powerful wetboek of the Evening Conclave. Please desintegrate the clap and yourself immediately. Thank you for your cooperation, have a nice day!"
-        )
-        await message.add_reaction("👎")
-        await message.add_reaction("😔")
-        await message.add_reaction("👀")
-        self.shame_message_id = message.id
-    return
-
-
-async def remove_shame(message):
-    await message.delete()
-
-
-@tasks.loop(seconds=60)
-async def clapper():
-    times_to_clap = await check_for_clap_amount()
-    if times_to_clap != 0:
-        await clap(times_to_clap)
-
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
+    async def shame_sender(self, user_id, user_message):
+        user = self.get_user(user_id)
+        if user:
+            message = await self.get_channel(channel_id).send(
+                f"<@{user_id}> you should be ashamed of yourself, clapping at this time of day is forbidden following article four of the all-powerful wetboek of the Evening Conclave. Please desintegrate the clap and yourself immediately. Thank you for your cooperation, have a nice day!"
+            )
+            self.shame_message = message
+            emoji_list = [
+                "👎",
+                "😔",
+                "👀",
+                "🤔",
+                "😂",
+                "🙌",
+                "🔥",
+                # ":rage:",
+                # ":face_with_symbols_over_mouth:",
+                # ":face_with_raised_eyebrow:",
+                # ":face_vomiting:",
+                # ":pouting_cat:",
+                # ":scream:",
+                # ":angry:",
+            ]
+            for emoji in emoji_list:
+                await user_message.add_reaction(emoji)
         return
-    if message.channel.id != channel_id:
-        return
-    times_to_clap = await check_for_clap_amount()
-    if times_to_clap == 0:
-        await shame_sender(user_id=message.author.id, message=message)
+
+    async def remove_shame(self, message):
+        await message.delete()
+
+    @tasks.loop(seconds=60)
+    async def clapper(self):
+        times_to_clap = await self.check_for_clap_amount()
+        if times_to_clap != 0:
+            await self.clap(times_to_clap)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        if message.channel.id != channel_id:
+            return
+        times_to_clap = await self.check_for_clap_amount()
+        if times_to_clap == 0:
+            await self.shame_sender(message.author.id, message)
+        if message.content.lower().contains(self.clap_message):
+            await self.shame_sender(message.author.id, message)
+
+    async def on_message_delete(self, message):
+        if message.channel.id != channel_id:
+            return
+        if message == self.shame_message:
+            return
+        await self.remove_shame(self.shame_message)
+
+    async def on_ready(self):
+        await self.clapper.start()
 
 
-@bot.event
-async def on_message_delete(message):
-    if message.channel.id != channel_id:
-        return
-    await remove_shame(message)
-
-
-@bot.event
-async def on_ready():
-    await clapper.start()
-
+bot = ClappyBoy(command_prefix="", intents=discord.Intents.all())
 
 bot.run(token)
